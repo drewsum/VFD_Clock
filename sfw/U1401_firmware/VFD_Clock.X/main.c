7f6d01e3aaefcbf67d652827a31b78c0b7a68906
Drew <drewmaatman2gmail.com>
2020-03-08 12:18:18 -0500

    Added MCU firmware project

---
+++ sfw/U1401_firmware/VFD_Clock.X/main.c
@@ -0,0 +1,22 @@
+/*
+ * File:   main.c
+ * Author: Drew Maatman
+ *
+ * Created on March 8th, 2020, 12:16 PM
+ */
+
+
+#include <xc.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "configuration.h"
+
+void main(void) {
+    
+    while (1) {
+        
+    }
+    
+}
